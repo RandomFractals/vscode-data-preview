514635ea7efea611dcbc1c11016b62f858d3de9e
Taras Novak <taras.novak@randomfractals.com>
2019-05-22 15:05:45 -0500

    switched to info log level for MVP release packaging

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 import { LogLevel } from "./logger";
 
 // log level setting for prod. vs. dev run of this ext.
-export const logLevel: LogLevel = LogLevel.Debug; // change to .Debug for ext. dev debug
+export const logLevel: LogLevel = LogLevel.Info; // change to .Debug for ext. dev debug
