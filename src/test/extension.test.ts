c50cc6c30802efdb6edf01bb985c7207fbcdc803
Taras Novak <taras.novak@randomfractals.com>
2019-06-03 11:18:28 -0500

    cleaned up ext. test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,22 +1,10 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
+import * as vscode from 'vscode';
 import * as assert from 'assert';
+// see https://mochajs.org/ for help
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-// import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
-
-// Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", function () {
-
-    // Defines a Mocha unit test
-    test("Something 1", function() {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
-    });
+suite("Data Preview Extension Tests", function() {
+  test("Should find extension", async () => {
+    const extension = await vscode.extensions.getExtension('RandomFractalsInc.vscode-data-preview');
+    assert(extension);
+  });
 });
