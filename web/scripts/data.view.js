6a72988152b367e324b08a0c89f07d76996b758d
Taras Novak <taras.novak@randomfractals.com>
2020-01-19 14:29:15 -0600

    #183 update stats on config changes only if not in restore mode

--- web/scripts/data.view.js
+++ web/scripts/data.view.js
@@ -124,8 +124,8 @@ function initializeDataView() {
 		viewer.addEventListener('perspective-config-update', event => {
 			if (!restoringConfig) {
 				updateConfig();
+				updateStats();
       }
-      updateStats();
 		});
 
 		// add viewer click handler for cross-filtering
@@ -173,7 +173,7 @@ function restoreConfig(viewConfig) {
 	logMessage(`restoreConfig(\n${JSON.stringify(viewConfig, null, 2)})`);
 	viewer.restore(viewConfig);
 	updateStats();
-	
+
 	// clear updating view config flag
 	restoringConfig = false;
 }
